aa228650e60da9216eef0628c18891c24ac057b3
Your Name <you@example.com>
2014-09-29 10:05:12 +0900

    add FindSnappy.cmake

---
+++ cmake/Modules/FindSnappy.cmake
@@ -0,0 +1,22 @@
+#FIindSnappy
+##
+
+IF(WIN32)
+ELSE(WIN32)
+  IF(APPLE)
+  ELSE(APPLE)
+    ##Linux
+    FIND_PATH(SNAPPY_INCLUDE_DIR NAMES snappy.h)
+
+    FIND_LIBRARY(SNAPPY_LIBRARY 
+      NAMES libsnappy.so
+      PATHS /usr/lib
+      )
+
+    SET(SNAPPY_LIBRARIES ${SNAPPY_LIBRARY})
+    SET(SNAPPY_INCLUDE_DIRS ${SNAPPY_INCLUDE_DIR})
+
+    INCLUDE(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Snappy SNAPPY_INCLUDE_DIR SNAPPY_LIBRARY)
+  ENDIF(APPLE)
+ENDIF(WIN32)
